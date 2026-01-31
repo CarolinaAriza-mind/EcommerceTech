1df62c24e396004e2a632daae25c6bff9644088f
CarolinaAT <carolinaarizat93@gmail.com>
2026-01-31 12:26:06 -0300

    Actualizando informacion para en deploy

--- ecommerce-carolina-ariza-mind/src/config/cloudinary.config.ts
+++ ecommerce-carolina-ariza-mind/src/config/cloudinary.config.ts
@@ -1,19 +1,18 @@
 import { v2 as cloudinary } from 'cloudinary';
-import { config as dotenvConfig } from 'dotenv';
-import { environment } from './enviroment.dev';
-
-dotenvConfig({ path: '.env.development.local' });
 
 export const CloudinaryConfig = {
   provide: 'CLOUDINARY',
   useFactory: () => {
-    console.log('Cloudinary conectado con:', {
-      cloud_name: environment.CLOUDINARY_CLOUD_NAME,
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
     });
-    return cloudinary.config({
-      cloud_name: environment.CLOUDINARY_CLOUD_NAME,
-      api_key: environment.CLOUDINARY_API_KEY,
-      api_secret: environment.CLOUDINARY_API_SECRET,
+
+    console.log('Cloudinary conectado:', {
+      cloud: process.env.CLOUDINARY_CLOUD_NAME,
     });
+
+    return cloudinary;
   },
 };
