1432e3f36336ac102e6270243ab8f1e6cab50d37
CarolinaAT <carolinaarizat93@gmail.com>
2026-02-25 12:25:30 -0300

    Cambio de entorno de produccion de Render a Railway, por eficiencia

--- ecommerce-carolina-ariza-mind/src/main.ts
+++ ecommerce-carolina-ariza-mind/src/main.ts
@@ -25,9 +25,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 5000);
-  console.log(
-    `Servidor escuchando en http://${process.env.HOST}:${process.env.PORT}`,
-  );
+  console.log(`Servidor escuchando en puerto 5000`);
 }
 bootstrap().catch((err) => {
   console.error('Error al iniciar la aplicación:', err);
